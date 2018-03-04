94d5eed0d7e235c54d5617e06ef8110c1aae2688
Huy Quang Nguyen <huyquang.nguyen@thomsonreuters.com>
2018-03-04 13:01:55 +1100

    Code First migration

---
+++ LibraryData/Migrations/20180303130433_Fix column Typos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryData.Migrations
+{
+    public partial class FixcolumnTypos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Descritpion",
+                table: "LibraryBranches",
+                newName: "Description");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "LibraryBranches",
+                newName: "Descritpion");
+        }
+    }
+}
