1da4640f267772369bcf008fdb313f598e294d66
Huy Quang Nguyen <hnguyenec@gmail.com>
2018-03-11 22:35:45 +1100

    Patron Index view and viewModels

---
+++ Library/Controllers/PatronController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library.Models.Patron;
+using LibraryData;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    public class PatronController: Controller
+    {
+        private readonly IPatron _patron;
+        
+        public PatronController(IPatron patron)
+        {
+            _patron = patron;
+        }
+
+        public IActionResult Index()
+        {
+            var allPatrons = _patron.GetAll();
+
+            var patronModels = allPatrons.Select(p => new PatronDetailModel
+            {
+                Id = p.Id,
+                FirstName = p.FirstName,
+                LastName = p.LastName,
+                LibraryCardId = p.LibraryCard.Id,
+                Address = p.Address,
+                MemberSince = p.LibraryCard.Created,
+                Telephone = p.TelephoneNumber,
+                HomeLibrary = p.HomeLibraryBranch.Name,
+                OverdueFees = p.LibraryCard.Fees,
+                AssetsCheckedOut = _patron.GetCheckouts(p.Id),
+                CheckoutHistory = _patron.GetCheckoutHistories(p.Id),
+                Holds = _patron.GetHolds(p.Id)
+            }).ToList();
+
+            var model = new PatronIndexModel()
+            {
+                Patrons = patronModels
+            };
+
+            return View(model);
+        }
+       
+    }
+}
---
+++ LibraryServices/PatronService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LibraryData;
+using LibraryData.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryServices
+{
+    public class PatronService: IPatron
+    {
+        private LibraryContext _context;
+        public PatronService(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        public Patron Get(int id)
+        {
+            return GetAll()
+                .FirstOrDefault(p => p.Id == id);
+        }
+
+        public IEnumerable<Patron> GetAll()
+        {
+            return _context.Patrons
+                .Include(p => p.LibraryCard)
+                .Include(p => p.HomeLibraryBranch);
+        }
+
+        public void Add(Patron newPatron)
+        {
+            _context.Add(newPatron);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<CheckoutHistory> GetCheckoutHistories(int patronId)
+        {
+            int cardId = GetPatronLibraryCardId(patronId);
+
+            return _context.CheckoutHistories
+                .Include(h => h.LibraryCard)
+                .Include(h => h.LibraryAsset)
+                .Where(h => h.LibraryCard.Id == cardId)
+                .OrderByDescending(h => h.CheckedOut);
+        }
+
+        private int GetPatronLibraryCardId(int patronId)
+        {
+            return _context.Patrons
+                            .Include(p => p.LibraryCard)
+                            .FirstOrDefault(p => p.Id == patronId)
+                            .LibraryCard.Id;
+        }
+
+        public IEnumerable<Hold> GetHolds(int patronId)
+        {
+            int cardId = GetPatronLibraryCardId(patronId);
+            return _context.Holds
+                .Include(hold => hold.LibraryCard)
+                .Include(hold => hold.LibraryAsset)
+                .Where(hold => hold.LibraryCard.Id == cardId)
+                .OrderByDescending(hold => hold.HoldPlaced);
+        }
+
+        public IEnumerable<Checkout> GetCheckouts(int patronId)
+        {
+            int cardId = GetPatronLibraryCardId(patronId);
+            return _context.Checkouts
+                .Include(co => co.LibraryCard)
+                .Include(co => co.LibraryAsset)
+                .Where(co => co.LibraryCard.Id == cardId);
+        }
+    }
+}
