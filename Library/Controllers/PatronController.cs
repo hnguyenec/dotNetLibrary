2f0470015af9ca7bcabe186b7d311f4abdb2485c
Huy Quang Nguyen <hnguyenec@gmail.com>
2018-03-11 22:47:38 +1100

    Patron Detail page

--- Library/Controllers/PatronController.cs
+++ Library/Controllers/PatronController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Library.Models.Patron;
 using LibraryData;
+using LibraryData.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Library.Controllers
@@ -42,6 +43,28 @@ namespace Library.Controllers
                 Patrons = patronModels
             };
 
+            return View(model);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var p = _patron.Get(id);
+
+            var model = new PatronDetailModel
+            {
+                Id = p.Id,
+                FirstName = p.FirstName,
+                LastName = p.LastName,
+                LibraryCardId = p.LibraryCard.Id,
+                Address = p.Address,
+                MemberSince = p.LibraryCard.Created,
+                Telephone = p.TelephoneNumber,
+                HomeLibrary = p.HomeLibraryBranch.Name,
+                OverdueFees = p.LibraryCard.Fees,
+                AssetsCheckedOut = _patron.GetCheckouts(p.Id).ToList() ?? new List<Checkout>(),
+                CheckoutHistory = _patron.GetCheckoutHistories(p.Id),
+                Holds = _patron.GetHolds(p.Id)
+            };
             return View(model);
         }
        
